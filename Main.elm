b39007f959484b365d3982fac0aa46c4c72b60a7
riya45 <56205260+riya45@users.noreply.github.com>
2020-01-24 10:58:49 -0800

    Update Main.elm
    removed" import Types exposing (Chat, Msg(PollMessages))" from line 10

--- Main.elm
+++ Main.elm
@@ -7,7 +7,7 @@ import Time exposing (second, every)
 import Model exposing (model)
 import Update exposing (update)
 import View exposing (view)
-import Types exposing (Chat, Msg(PollMessages))
+
 
 
 init = (model, Cmd.none)
